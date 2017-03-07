3edfaa3dc846094db68f5f1a633df5e3c3449c4b
茂木雄一郎 <mogi.3614@gmail.com>
2017-03-07 22:16:56 +0900

    終わり

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,4 +8,28 @@ class Book < ActiveRecord::Base
   validates :name, presence: true
   validates :name, length: { maximum: 15 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates do |book|
+    if book.name.include?("exercise")
+      book.errors[:name] << "I don't like exercise."
+    end
+  end
+
+  befor_validation do |book|
+    book.name = book.name.gsub(/Cat/) do |matched|
+      "lovely#{matched}"
+    end
+  end
+
+  after_destroy do |book|
+    Rails.logger.info "Book is deleted: #{book.attributes.inspect}"
+  end
+
+  def high_price?
+    price >= 5000
+  end
+
+  after_destroy :if => :high_price? do |book|
+    Rails.logger.warn "Book with high price is deleted: #{book.attributes.inspect}"
+    Rails.logger.warn "please check!!"
+  end
 end
